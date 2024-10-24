24facd2da1f96dd2db2e0dbe812ded6a99acd908
PNorrmyr <philip.norrmyr@gmail.com>
2024-10-24 15:41:41 +0200

    Added navigation to pages

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -1,11 +1,20 @@
+import { Link } from 'react-router-dom';
 
-
-function LoadingPage(){
-
-    return (
-
-        <p>LoadingPage</p>
-    )
+function LoadingPage() {
+  return (
+    <div>
+      <h1>Loading Page</h1>
+      <nav>
+        <ul>
+          <li><Link to="/settimer">Go to Set Timer</Link></li>
+          <li><Link to="/analogtimer">Go to Analog Timer</Link></li>
+          <li><Link to="/digitaltimer">Go to Digital Timer</Link></li>
+          <li><Link to="/alarmview">Go to Alarm View</Link></li>
+          <li><Link to="/pauseview">Go to Pause View</Link></li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
-export default LoadingPage
+export default LoadingPage;
